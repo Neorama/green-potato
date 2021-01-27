edb9295c3df6b598e14fdc81e314410794687efe
sivakumar <siva.kalagara@strsi.com>
2021-01-27 18:54:04 +0530

    header and footer changed

--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -8,17 +8,38 @@ export default class Submenu extends React.Component {
     render() {
         let page = _.get(this.props, 'page', null);
         return (
-            <ul className={_.get(this.props, 'menu_class', null)}>
+          <div className={_.get(this.props, 'menu_class', null)}>
+            <div className="container">
+
+            <ul className="row ">
+              
             {_.map(_.get(this.props, 'submenu', null), (action, action_idx) => {
                 let page_url = _.trim(_.get(page, 'url', null), '/');
                 let action_url = _.trim(_.get(action, 'url', null), '/');
                 return (
-                  <li key={action_idx} className={classNames('menu-item', {'current': page_url === action_url, 'menu-button': _.get(action, 'style', null) !== 'link'})}>
-                    <ActionLink {...this.props} action={action} />
+                  <li key={action_idx} className={classNames('menu-item',"col-sm-4", {'current': page_url === action_url, 'menu-button': _.get(action, 'style', null) !== 'link'})}>
+                    <a className="row cardlink" href={_.get(action, 'url', null)}>
+                   {_.get(action, 'icon', null) && 
+                      (<div className="col-md-3 p-0 pl-2 linkIcon">
+                      <img src={_.get(action, 'icon', null)}/>
+                    </div>)
+                   } 
+                    <div className="col-md-9 textwrap">
+                      <h6>{_.get(action, 'label', null)}</h6>
+                      {_.get(action, 'content', null) && 
+                      ( <p>{_.get(action, 'content', null)}</p>)
+                   } 
+                     
+                      
+                    </div>
+                    </a>
+                    {/* <ActionLink {...this.props} action={action} /> */}
                   </li>
                 )
             })}
             </ul>
+            </div>
+            </div>
         );
     }
 }
